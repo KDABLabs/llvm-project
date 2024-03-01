dcbbe24b93612409c386407b39e31a7447a541e0
Cristian Adam <cristian.adam@gmail.com>
2024-03-01 11:21:27 +0100

    [clang-format] Fix crash in TokenAnnotator #82349
    Squash commit from https://github.com/llvm/llvm-project/pull/82349
    
    Change-Id: Ie2a8d501486f2132e5ab9db57159a01f1b631fec

--- clang/unittests/Format/FormatTest.cpp
+++ clang/unittests/Format/FormatTest.cpp
@@ -13422,6 +13422,12 @@ TEST_F(FormatTest, IncorrectCodeUnbalancedBraces) {
   verifyFormat("{");
   verifyFormat("#})");
   verifyNoCrash("(/**/[:!] ?[).");
+  verifyNoCrash("struct X {\n"
+                "  operator iunt(\n"
+                "};");
+  verifyNoCrash("struct Foo {\n"
+                "  operator foo(bar\n"
+                "};");
 }
 
 TEST_F(FormatTest, IncorrectUnbalancedBracesInMacrosWithUnicode) {
